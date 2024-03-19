3dda529e0f30c81396a782914319e8250003b6a9
antonileon <tonijleon@gmail.com>
2024-03-19 18:54:58 -0300

    creacion de archivos para template general

---
+++ resources/views/layouts/css.blade.php
@@ -0,0 +1,23 @@
+<!-- META DATA -->
+<meta charset="UTF-8">
+<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="">
+<meta name="author" content="">
+<meta name="keywords" content="">
+
+<!-- FAVICON -->
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/brand/favicon.ico') }}"/>
+
+<!-- TITLE -->
+<title>{{ config('app.name') }} | @yield('title')</title>
+
+<!-- BOOTSTRAP CSS -->
+<link id="style" href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
+
+<!-- STYLE CSS -->
+<link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/skin-modes.css') }}" rel="stylesheet" />
+
+<!--- FONT-ICONS CSS -->
+<link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" />
